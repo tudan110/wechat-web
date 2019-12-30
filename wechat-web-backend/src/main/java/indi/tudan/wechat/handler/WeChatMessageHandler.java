78734b07435008e60f3e386754180a2d45634bed
tudan110 <wtudan@126.com>
2019-12-30 14:30:34 +0800

    移动代码

--- wechat-web-backend/src/main/java/indi/tudan/wechat/handler/WeChatMessageHandler.java
+++ wechat-web-backend/src/main/java/indi/tudan/wechat/handler/WeChatMessageHandler.java
@@ -1,7 +1,17 @@
 package indi.tudan.wechat.handler;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
 import cn.zhouyafeng.itchat4j.beans.BaseMsg;
 import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import indi.tudan.wechat.config.TulingConfig;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 微信消息处理
@@ -10,45 +20,121 @@ import cn.zhouyafeng.itchat4j.face.IMsgHandlerFace;
  * @date 2019-12-03 14:44:14
  * @since 1.0
  */
+@Slf4j
 public class WeChatMessageHandler implements IMsgHandlerFace {
 
     @Override
-    public String textMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String textMsgHandle(BaseMsg msg) {
+
+        // 获取帮助信息
+        String help = TulingConfig.getHelp(msg);
+        if (StrUtil.isNotBlank(help)) {
+            return help;
+        }
+
+        // 添加或移除白名单，并返回问候语
+        String greetings = TulingConfig.addOrRemoveWhiteList(msg);
+        if (StrUtil.isNotBlank(greetings)) {
+            return greetings;
+        }
+
+        // 不在白名单里，就返回空
+        if (TulingConfig.isNotInWhitelist(msg)) {
+            return "";
+        }
+
+        // 返回消息内容
+        String result = "";
+
+        /* start: 封装 api 参数 */
+        JSONObject param = new JSONObject();
+        param.put("reqType", 0);
+        param.put("perception",
+                new JSONObject().fluentPut("inputText",
+                        new JSONObject().fluentPut("text", msg.getText())));
+        param.put("userInfo",
+                new JSONObject()
+                        .fluentPut("apiKey", TulingConfig.getTulingApiKey())
+                        .fluentPut("userId", TulingConfig.getTulingUserId()));
+        /* end: 封装 api 参数 */
+
+        try {
+
+            // 请求图灵机器人接口
+            JSONObject response = JSON.parseObject(HttpUtil.post(TulingConfig.getTulingUrl(), JSON.toJSONString(param)));
+
+            JSONArray results = response.getJSONArray("results");
+            List<String> stringList = new ArrayList<>();
+            for (int i = 0; i < results.size(); i++) {
+                stringList.add(results.getJSONObject(i).getJSONObject("values").getString("text"));
+            }
+            result = String.join("，", stringList);
+
+        } catch (Exception e) {
+            log.error("服务器解析错误，请联系管理员。", e);
+            result = StrUtil.format("{}。\nstack info: {}",
+                    "服务器解析错误，请联系土旦",
+                    e.getMessage());
+        }
+        return result;
     }
 
     @Override
-    public String picMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String picMsgHandle(BaseMsg msg) {
+
+        // 不在白名单里，就返回空
+        if (TulingConfig.isNotInWhitelist(msg)) {
+            return "";
+        }
+
+        return "收到图片";
     }
 
     @Override
-    public String voiceMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String voiceMsgHandle(BaseMsg msg) {
+
+        // 不在白名单里，就返回空
+        if (TulingConfig.isNotInWhitelist(msg)) {
+            return "";
+        }
+
+        //String fileName = String.valueOf(System.currentTimeMillis());
+        //String voicePath = "D://itchat4j/voice" + File.separator + fileName + ".mp3";
+        //DownloadTools.getDownloadFn(msg, MsgTypeEnum.VOICE.getType(), voicePath);
+        return "收到语音";
     }
 
     @Override
-    public String viedoMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String viedoMsgHandle(BaseMsg msg) {
+
+        // 不在白名单里，就返回空
+        if (TulingConfig.isNotInWhitelist(msg)) {
+            return "";
+        }
+
+        //String fileName = String.valueOf(System.currentTimeMillis());
+        //String viedoPath = "D://itchat4j/viedo" + File.separator + fileName + ".mp4";
+        //DownloadTools.getDownloadFn(msg, MsgTypeEnum.VIEDO.getType(), viedoPath);
+        return "收到视频";
     }
 
     @Override
-    public String nameCardMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String nameCardMsgHandle(BaseMsg msg) {
+        return null;
     }
 
     @Override
-    public void sysMsgHandle(BaseMsg baseMsg) {
-
+    public void sysMsgHandle(BaseMsg msg) {
     }
 
     @Override
-    public String verifyAddFriendMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String verifyAddFriendMsgHandle(BaseMsg msg) {
+        return null;
     }
 
     @Override
-    public String mediaMsgHandle(BaseMsg baseMsg) {
-        return "";
+    public String mediaMsgHandle(BaseMsg msg) {
+        return null;
     }
+
 }
